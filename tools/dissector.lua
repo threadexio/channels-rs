fca672dde642708665798785e4344a31bfcb06cf
threadexio <pzarganitis@gmail.com>
2023-05-28 19:47:58 +0300

    feat(dissector): group packets based on chunks

--- tools/dissector.lua
+++ tools/dissector.lua
@@ -9,6 +9,8 @@ proto.fields.flags    = ProtoField.uint8("channels.flags",     "Flags",    base.
 proto.fields.id       = ProtoField.uint8("channels.id",        "ID")
 proto.fields.payload  = ProtoField.bytes ("channels.payload",  "Payload",  base.SPACE)
 
+packet_is_in_group = false
+bytes_left_in_payload = nil
 
 function proto.dissector(buf, pinfo, tree)
 	pinfo.cols.protocol = "channels-rs"
@@ -20,10 +22,27 @@ function proto.dissector(buf, pinfo, tree)
 	local flags    = buf(6, 1)
 	local id       = buf(7, 1)
 
-	pinfo.cols.info:prepend("ID=" .. id:uint() .. " ")
-
 	local header_length = 8
 	local payload = buf(header_length)
+
+	pinfo.cols.info:prepend("ID=" .. id:uint() .. " ")
+
+	if bytes_left_in_payload ~= nil then
+		if bytes_left_in_payload <= 0 then
+			bytes_left_in_payload = nil
+		else
+			bytes_left_in_payload = bytes_left_in_payload - buf:len()
+
+			pinfo.cols.info:prepend("**Ignore** ")
+		end
+	else
+		if buf:len() < length:uint() then
+			-- this means that the channels packet was split up
+			bytes_left_in_payload = length:uint() - buf:len()
+		end
+	end
+
+	local more_data_flag_set = bitand(flags:uint(), bitshl(1, 7)) ~= 0
 
 	subtree:add_packet_field(proto.fields.version, version, ENC_BIG_ENDIAN)
 
@@ -34,25 +53,35 @@ function proto.dissector(buf, pinfo, tree)
 	subtree:add_packet_field(proto.fields.checksum, checksum, ENC_BIG_ENDIAN)
 
 	local flags_tree = subtree:add_packet_field(proto.fields.flags, flags, ENC_BIG_ENDIAN)
-	add_flag_to_subtree(flags_tree, flags, "More Data", 7)
-	add_flag_to_subtree(flags_tree, flags, "Reserved bit", 6)
-	add_flag_to_subtree(flags_tree, flags, "Reserved bit", 5)
-	add_flag_to_subtree(flags_tree, flags, "Reserved bit", 4)
-	add_flag_to_subtree(flags_tree, flags, "Reserved bit", 3)
-	add_flag_to_subtree(flags_tree, flags, "Reserved bit", 2)
-	add_flag_to_subtree(flags_tree, flags, "Reserved bit", 1)
-	add_flag_to_subtree(flags_tree, flags, "Reserved bit", 0)
+	add_flag_to_subtree(flags_tree, "More Data", 7, more_data_flag_set)
 
 	subtree:add_packet_field(proto.fields.id,       id,       ENC_BIG_ENDIAN)
 	subtree:add_packet_field(proto.fields.payload,  payload,  ENC_BIG_ENDIAN)
+
+	local packet_group_prefix = ""
+	if more_data_flag_set then
+		if packet_is_in_group then
+			packet_group_prefix = "│ "
+		else
+			packet_group_prefix = "┌ "
+			packet_is_in_group = true
+		end
+	else
+		if packet_is_in_group then
+			packet_group_prefix = "└ "
+			packet_is_in_group = false
+		else
+			packet_group_prefix = "· "
+		end
+	end
+	pinfo.cols.info:prepend(packet_group_prefix)
 end
 
 tcp_table = DissectorTable.get("tcp.port")
 tcp_table:add(10000, proto)
 tcp_table:add(10001, proto)
 
-function add_flag_to_subtree(tree, flags, name, bit)
-	local is_set = bitand(flags:uint(), bitshl(1, bit)) ~= 0
+function add_flag_to_subtree(tree, name, bit, is_set)
 	local result = ""
 
 	local lpad = 7 - bit
