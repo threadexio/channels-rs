e683a6bbfb9c3fdb113250e378c407b43f38bef1
threadexio <pzarganitis@gmail.com>
2024-05-20 14:11:17 +0300

    docs: add docs for `WriteTransaction`

--- channels-io/src/transaction.rs
+++ channels-io/src/transaction.rs
@@ -7,7 +7,23 @@ use alloc::vec::Vec;
 
 use crate::{AsyncWrite, Write};
 
+/// An abstraction that allows bunching up many [`write()`] calls into one.
+///
+/// This abstraction can hold a reference to a [`Vec<u8>`] in which it will
+/// write data given to it from the [`Write`] and [`AsyncWrite`] it implements.
+/// Upon calling [`finish_sync()`] or [`finish_async()`] it will write the contents
+/// of that buffer to the underlying writer. It must also be noted that if configured
+/// to do buffering, the [`WriteTransaction`] will also batch calls to [`flush()`]
+/// and call it once, if needed, when finished. When a buffer is not provided, it
+/// passes through the [`Write`] and [`AsyncWrite`] implementations to the underlying
+/// writer in a "1-1" manner.
+///
+/// [`write()`]: fn@Write::write
+/// [`flush()`]: fn@Write::flush
+/// [`finish_sync()`]: fn@WriteTransaction::finish_sync
+/// [`finish_async()`]: fn@WriteTransaction::finish_async
 #[derive(Debug)]
+#[must_use = "transactions should always be `.finish_*()`ed"]
 pub struct WriteTransaction<'a, W: ?Sized> {
 	writer: &'a mut W,
 	buf: Option<&'a mut Vec<u8>>,
@@ -15,20 +31,27 @@ pub struct WriteTransaction<'a, W: ?Sized> {
 }
 
 impl<'a, W: ?Sized> WriteTransaction<'a, W> {
+	/// Create a new transaction that uses `buf` to buffer up writes.
+	///
+	/// This method will also clear `buf`.
 	pub fn buffered(writer: &'a mut W, buf: &'a mut Vec<u8>) -> Self {
+		buf.clear();
 		Self { writer, buf: Some(buf), wants_flush: false }
 	}
 
+	/// Create a new transaction that does no buffering.
 	pub fn unbuffered(writer: &'a mut W) -> Self {
 		Self { writer, buf: None, wants_flush: false }
 	}
 
+	/// Get a reference to the underlying writer.
 	#[inline]
 	#[must_use]
 	pub fn writer(&self) -> &W {
 		self.writer
 	}
 
+	/// Get a mutable reference to the underlying writer.
 	#[inline]
 	#[must_use]
 	pub fn writer_mut(&mut self) -> &mut W {
@@ -64,6 +87,18 @@ impl<'a, W: Write + ?Sized> Write for WriteTransaction<'a, W> {
 }
 
 impl<'a, W: Write + ?Sized> WriteTransaction<'a, W> {
+	/// Finish the transaction.
+	///
+	/// If the transaction does buffering then this method will attempt to write
+	/// the whole buffer to the writer. Additionally, if [`flush()`] was ever
+	/// called on the transaction, then this method will also attempt to flush
+	/// the writer after writing to it. However, if the transaction is was created
+	/// with [`unbuffered()`] then this method is simply a no-op. It is semantically
+	/// correct that a [`WriteTransaction`] is always finished regardless of whether
+	/// the finish operation does any work.
+	///
+	/// [`flush()`]: fn@Write::flush
+	/// [`unbuffered()`]: fn@WriteTransaction::unbuffered
 	pub fn finish_sync(self) -> Result<(), W::Error> {
 		let Self { buf, wants_flush, writer } = self;
 
@@ -116,6 +151,18 @@ impl<'a, W: AsyncWrite + ?Sized> AsyncWrite
 }
 
 impl<'a, W: AsyncWrite + ?Sized> WriteTransaction<'a, W> {
+	/// Finish the transaction.
+	///
+	/// If the transaction does buffering then this method will attempt to write
+	/// the whole buffer to the writer. Additionally, if [`flush()`] was ever
+	/// called on the transaction, then this method will also attempt to flush
+	/// the writer after writing to it. However, if the transaction is was created
+	/// with [`unbuffered()`] then this method is simply a no-op. It is semantically
+	/// correct that a [`WriteTransaction`] is always finished regardless of whether
+	/// the finish operation does any work.
+	///
+	/// [`flush()`]: fn@AsyncWrite::flush
+	/// [`unbuffered()`]: fn@WriteTransaction::unbuffered
 	pub async fn finish_async(self) -> Result<(), W::Error> {
 		let Self { buf, wants_flush, writer } = self;
 
